b100b1f607ba6e2a9e7ad39f29d76faf56e06e33
Funmilayo Amusan <funmilayo@healthtracka.com>
2025-03-10 22:35:21 +0100

    server setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from "cors";
+import http from 'http';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { Server } from "socket.io";
+
+
+dotenv.config();
+
+const app = express()
+app.use(express.json())
+app.use(cors());
+app.use(helmet())
+const server = http.createServer(app);
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
