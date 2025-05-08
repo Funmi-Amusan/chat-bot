2c5105f4d6a5bf82652df7dcec471eba8572f74d
funmi <maryamusan77@gmail.com>
2025-05-08 20:47:25 +0100

    add text to banner for lg screens

--- client/src/components/home/Banner.tsx
+++ client/src/components/home/Banner.tsx
@@ -3,10 +3,11 @@ import { IconAssets } from "@/assets/icons";
 
 export const Banner = () => {
   return (
-    <section className=" h-12 w-full rainbow-gradient flex items-center justify-center gap-4">
-        <p className=" text-body-tag ">View the complete kit</p>
+    <a href="" className=" py-3 w-full rainbow-gradient flex items-center justify-center gap-2">
+      <p className=" text-body-tag hidden md:block">This page is included in a free SaaS Website Kit.</p>
+        <p className=" text-body-tag" >View the complete kit</p>
         <img src={IconAssets.arrowBlack.src} alt="arrow right icon" width={16} height={16} />
-    </section>
+    </a>
   )
   ;
 };
